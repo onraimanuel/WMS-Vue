836def72a84bb361d4c47e3f3b7e10bf82c95222
Onra Imanuel Sihombing <onraimanuel@MacBook-Pro-Onra.local>
2024-04-10 22:39:17 +0700

    Perbaikan 10 Apr

--- resources/js/components/SearchInput.vue
+++ resources/js/components/SearchInput.vue
@@ -1,45 +1,51 @@
 <template>
-  <input type="text" v-model="searchText" @input="handleInput" :placeholder="placeholder" class="form-control mb-3" />
+    <input
+        type="text"
+        v-model="searchText"
+        @input="handleInput"
+        :placeholder="placeholder"
+        class="form-control mb-3"
+    />
 </template>
 
 <script>
 export default {
-  props: {
-    value: {
-      type: String,
-      default: '',
+    props: {
+        value: {
+            type: String,
+            default: "",
+        },
+        placeholder: {
+            type: String,
+            default: "Search",
+        },
+        dataToSearch: {
+            type: Array,
+            default: () => [],
+        },
     },
-    placeholder: {
-      type: String,
-      default: 'Search',
+    data() {
+        return {
+            searchText: this.value,
+        };
     },
-    dataToSearch: {
-      type: Array,
-      default: () => [],
+    watch: {
+        value(newVal) {
+            this.searchText = newVal;
+        },
     },
-  },
-  data() {
-    return {
-      searchText: this.value,
-    };
-  },
-  watch: {
-    value(newVal) {
-      this.searchText = newVal;
+    methods: {
+        handleInput() {
+            this.$emit("input", this.searchText);
+            this.$emit("search-input", this.searchText);
+        },
     },
-  },
-  methods: {
-    handleInput() {
-      this.$emit('input', this.searchText);
-      this.$emit('search-input', this.searchText);
-    },
-  },
 };
 </script>
 
 <style scoped>
 .form-control mb-3 {
-  border-color: #000000;
-  /* Sesuaikan warna border dengan warna latar belakang */
+    border-color: #000000;
+    /* Sesuaikan warna border dengan warna latar belakang */
 }
 </style>
